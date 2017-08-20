73e0cf6820e9be3388c1126914e56758aa4c46fe
NihaoRay <NihaoRay@users.noreply.github.com>
2017-08-20 17:01:41 +0800

    添加sql表结构
    添加了字典表，角色表，角色用户表，用户表，登录日志表，

---
+++ sql/score.sql
@@ -0,0 +1,121 @@
+/*
+SQLyog Ultimate v11.5 (64 bit)
+MySQL - 5.6.20 : Database - score
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`score` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `score`;
+
+/*Table structure for table `login_log` */
+
+DROP TABLE IF EXISTS `login_log`;
+
+CREATE TABLE `login_log` (
+  `id` varchar(64) DEFAULT NULL,
+  `user_id` varchar(64) DEFAULT NULL COMMENT '用户id',
+  `login_ip` varchar(64) DEFAULT NULL COMMENT '登录的ip',
+  `login_date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `login_flag` char(1) DEFAULT NULL,
+  `del_flag` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `login_log` */
+
+/*Table structure for table `sys_dict` */
+
+DROP TABLE IF EXISTS `sys_dict`;
+
+CREATE TABLE `sys_dict` (
+  `id` varchar(64) NOT NULL,
+  `value` varchar(64) DEFAULT NULL COMMENT '键值',
+  `label` varchar(64) DEFAULT NULL COMMENT '字典的值',
+  `type` varchar(64) DEFAULT NULL COMMENT '类型',
+  `description` varchar(128) DEFAULT NULL COMMENT '描述',
+  `sort` varchar(64) DEFAULT NULL COMMENT '字段修改',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建人',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `remarks` varchar(128) DEFAULT NULL COMMENT '备注',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '修改人',
+  `update_date` datetime DEFAULT NULL COMMENT '修改时间',
+  `del_flag` char(1) DEFAULT NULL COMMENT '删除标志位',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='字典表';
+
+/*Data for the table `sys_dict` */
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` varchar(64) DEFAULT NULL,
+  `office_id` varchar(64) DEFAULT NULL COMMENT '归属机构',
+  `name` varchar(64) DEFAULT NULL COMMENT '角色名称',
+  `enname` varchar(64) DEFAULT NULL COMMENT '角色英语名称',
+  `role_type` varchar(64) DEFAULT NULL COMMENT '角色类型',
+  `data_sope` varchar(64) DEFAULT NULL,
+  `is_sys` char(1) DEFAULT NULL COMMENT '是否管理员',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建人',
+  `create_date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '创建日期',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '修改人',
+  `update_date` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000' COMMENT '修改日期',
+  `remarks` varchar(128) DEFAULT NULL COMMENT '备注',
+  `del_flag` char(1) DEFAULT NULL COMMENT '删除标志位'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限控制表';
+
+/*Data for the table `sys_role` */
+
+/*Table structure for table `sys_role_user` */
+
+DROP TABLE IF EXISTS `sys_role_user`;
+
+CREATE TABLE `sys_role_user` (
+  `id` varchar(64) DEFAULT NULL,
+  `user_id` varchar(64) DEFAULT NULL COMMENT '关联到用户表',
+  `role_id` varchar(64) DEFAULT NULL COMMENT '关联到角色表'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_role_user` */
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` varchar(64) DEFAULT NULL,
+  `area` varchar(128) DEFAULT NULL COMMENT '地址',
+  `company` varchar(64) DEFAULT NULL COMMENT '公司',
+  `login_name` varchar(64) DEFAULT NULL COMMENT '登录名称',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
+  `no` varchar(64) DEFAULT NULL COMMENT '编号',
+  `name` varchar(64) DEFAULT NULL COMMENT '名称',
+  `email` varchar(64) DEFAULT NULL COMMENT '邮编',
+  `phone` varchar(64) DEFAULT NULL COMMENT '电话号码',
+  `mobile` varchar(64) DEFAULT NULL COMMENT '手机号码',
+  `user_type` varchar(64) DEFAULT NULL COMMENT '用户类型',
+  `photo_path` varchar(64) DEFAULT NULL COMMENT '用户头像',
+  `login_log_id` varchar(64) DEFAULT NULL COMMENT '用户登录记录日志',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '创建日期',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '修改者',
+  `update_date` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000' COMMENT '修改日期',
+  `remarks` varchar(128) DEFAULT NULL COMMENT '备注',
+  `del_flag` char(1) DEFAULT NULL COMMENT '删除标志位'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
